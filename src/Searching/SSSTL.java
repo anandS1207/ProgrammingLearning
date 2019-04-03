e880dd9fc57ca12ff084404261df4a192c36a47e
anandS1207 <sahukari.anandarao@gmail.com>
2019-04-03 16:31:04 +0530

    #Task:
    Find a sorted subsequence of size 3 in linear time

---
+++ src/Searching/SSSTL.java
@@ -0,0 +1,57 @@
+package Searching;
+
+public class SSSTL {
+
+	public static void main(String[] args) {
+		int arr[] = { 12, 11, 10, 5, 20, 6, 2, 30, 35, 3, 6, 10, 15 };
+		find3Numbers(arr);
+	}
+
+	static void find3Numbers(int arr[]) {
+		int n = arr.length;
+		int max = n - 1; // Index of maximum element from right side
+		int min = 0; // Index of minimum element from left side
+		int i;
+		int[] smaller = new int[n];
+		smaller[0] = -1; // first entry will always be -1
+		System.out.print("smaller array : " + smaller[0]);
+		for (i = 1; i < n; i++) {
+			if (arr[i] <= arr[min]) {
+				min = i;
+				smaller[i] = -1;
+			} else
+				smaller[i] = min;
+			System.out.print(" " + smaller[i]);
+		}
+
+		// Create another array that will store index of a
+		// greater element on right side. If there is no greater
+		// element on right side, then greater[i] will be -1.
+		int[] greater = new int[n];
+		greater[n - 1] = -1; // last entry will always be -1
+		System.out.println();
+		System.out.print("Greater array in reverse Order: " + greater[n - 1]);
+		for (i = n - 2; i >= 0; i--) {
+			if (arr[i] >= arr[max]) {
+				max = i;
+				greater[i] = -1;
+			} else
+				greater[i] = max;
+			System.out.print(" " + greater[i]);
+		}
+
+		// Now find a number which has both a greater number
+		// on right side and smaller number on left side
+		System.out.println();
+		for (i = 0; i < n; i++) {
+			if (smaller[i] != -1 && greater[i] != -1) {
+				System.out.print(arr[smaller[i]] + " " + arr[i] + " " + arr[greater[i]]);
+				return;
+			}
+		}
+
+		// If we reach number, then there are no such 3 numbers
+		System.out.println("No such triplet found");
+		return;
+	}
+}
