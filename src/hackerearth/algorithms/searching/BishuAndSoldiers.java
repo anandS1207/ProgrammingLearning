70c02445ffd0e832ac41612d37fb9abe090874a6
sahukari.anandarao@gmail.com <sahukari.ananda.rao@accenture.com>
2019-11-05 20:54:23 +0530

    as of now liear search very easy and easy has solved

---
+++ src/hackerearth/algorithms/searching/BishuAndSoldiers.java
@@ -0,0 +1,17 @@
+package hackerearth.algorithms.searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BishuAndSoldiers {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		int a[] = new int[t];
+		for (int i = 0; i < t; i++) {
+			a[i] = s.nextInt();
+		}
+		Arrays.sort(a);
+		int search = s.nextInt();
+	}
+}
--- src/hackerearth/algorithms/searching/RestInPeace.java
+++ src/hackerearth/algorithms/searching/RestInPeace.java
@@ -1,10 +1,23 @@
 package hackerearth.algorithms.searching;
 
+import java.util.Scanner;
+
 public class RestInPeace {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		Long m;
+		for (int i = 0; i < t; i++) {
+			m = s.nextLong();
+			if (String.valueOf(m).contains("21"))
+				System.out.println("The streak is broken!");
+			else if (m % 3 == 0 && m % 7 == 0) {
+				System.out.println("The streak is broken!");
+			} else
+				System.out.println("The streak lives still in our heart!");
+		}
 	}
 
 }
